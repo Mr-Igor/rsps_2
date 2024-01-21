883ca08dc788919213d0969e93e3e1f75f540536
Mr-Igor <59827408+Mr-Igor@users.noreply.github.com>
2024-01-21 20:21:56 +0300

    Add files via upload

---
+++ lab1_E/View/WindowCurrency.xaml.cs
@@ -0,0 +1,32 @@
+﻿using lab1_E.Model;
+using lab1_E.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1_E.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowCurrency.xaml
+    /// </summary>
+    public partial class WindowCurrency : Window
+    {
+        public WindowCurrency()
+        {
+            InitializeComponent();
+            CurrencyViewModel vmViewModel = new CurrencyViewModel();
+            ListCurrency.ItemsSource = vmViewModel.ListCurrency;
+        }
+    }
+}
---
+++ lab1_E/ViewModel/CurrencyViewModel.cs
@@ -0,0 +1,21 @@
+﻿using lab1_E.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_E.ViewModel
+{
+    class CurrencyViewModel
+    {
+        public ObservableCollection<Currency> ListCurrency { get; set; } = new ObservableCollection<Currency>();
+
+        public CurrencyViewModel()
+        {
+            this.ListCurrency.Add(new Currency(1, "Доллар", "USD"));
+            this.ListCurrency.Add(new Currency(2, "Евро", "EUR"));
+        }
+    }
+}
